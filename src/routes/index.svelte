d92441143435c93dd1b736da5302fce5f2cb348d
Mark Phillips <me@compuglobalhypermegacorp.com>
2021-01-13 07:16:23 -0800

    Added a few additional pages

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,7 +8,8 @@
 	}
 </style>
 
-<div class:centerer={true}> <!-- Here's how you could extract conditional class groups with @apply -->
+<div class:centerer={true}>
+	<!-- Here's how you could extract conditional class groups with @apply -->
 	<ExampleComponent
 		title="🌐 Opinionated Sapper project base"
 		paragraph="This is an example route and component to make sure everything's working." />
@@ -17,7 +18,7 @@
 		class="mt-10 p-3 rounded-lg shadow-md text-pink-800 bg-pink-200 transition
 		duration-200 ease-in-out hover:bg-pink-300 focus:bg-pink-300
 		focus:outline-none focus:shadow-outline"
-		href="/graphql">
-		Check out the GraphQL playground!
+		href="/about">
+		About
 	</a>
 </div>
